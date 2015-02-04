ff45bc4443f8e9246fdfc487ff2c4d5951840afc
Matthew Lee <ml7@uw.edu>
2015-02-04 14:38:07 -0800

    Added tests for linked_list class __init__, size(), insert(), pop() and search().

--- test_linked_list.py
+++ test_linked_list.py
@@ -1,5 +1,6 @@
 # test constructor
 from linked_list import list_node
+from linked_list import linked_list
 
 
 def test_constructor_ln():
@@ -16,14 +17,53 @@ def test_node_attributes():
     assert a.next
     assert a.next.data == 3
 
-# insert(val)
 
-# pop()
+def test_linked_list_cons():
+    # Verify that an empty linked list is created.
+    a = linked_list()
+    assert isinstance(a, linked_list)
+    assert a.head is None
+
+# Test creating a populated linked list.
+
+
+# Test insert(val) and pop()
+def test_insert_pop():
+    b = linked_list()
+    b.insert(5)
+    assert b.head.data is 5
+    b.pop()
+    assert b.head is None
+
 
 # size()
+def test_size():
+    s = linked_list()
+    assert s.size() is 0
+    for something in range(100):
+        s.insert(something)
+    assert s.size() is 100
+
 
 # search(val)
+def test_search():
+    a = linked_list()
+    a.insert('a')
+    a.insert(1)
+    assert a.search(1) is 1
+    assert a.search('a') is 'a'
+
 
 # remove(node)
+def test_remove():
+    pass
+
 
 # print()
+def test_print():
+    pass
+
+
+# test __str__
+def test_str():
+    pass
