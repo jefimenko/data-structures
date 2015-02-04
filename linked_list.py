a13d59e40d1d53f255c0b9f8936e60ff2bcfaa5c
Edward <edpark13@gmail.com>
2015-02-04 11:59:09 -0800

    created linked_list.py and add list_node class and linked_list

---
+++ linked_list.py
@@ -0,0 +1,13 @@
+#class for list node
+class list_node(object):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+class link_list(object):
+    def __init__(self, *args):
+        self.head = None
+        #if args:
+            #self.head = args[0]
+
+
