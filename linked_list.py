61c360cd9105bd130f13c9e2d58cce1e815dc46b
Matthew Lee <ml7@uw.edu>
2015-02-04 12:48:30 -0800

    Added insert() and pop() functions to linked_list class.

--- linked_list.py
+++ linked_list.py
@@ -1,14 +1,28 @@
-#class for list node
+# class for list node
 class list_node(object):
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
-#class for linked list
+# class for linked list
 class linked_list(object):
     def __init__(self, *args):
         self.head = None
-        #if args:
-            #self.head = args[0]
+        # if args:
+        #    self.head = args[0]
 
+    def insert(self, val):
+        """
+        From a given val, create a new node with .data attribute set to val
+        and rearrange the list so the head is the new node with a reference
+        to the old head.
+        """
+        new_node = list_node(val, self.head)
+        self.head = new_node
 
+    def pop(self):
+        """
+        Remove the item at the head of the linked list, and rearrange
+        accordingly.
+        """
+        self.head = self.head.next
