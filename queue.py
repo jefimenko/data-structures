a9bbc44472234d2676c08466a8230b078a4212f5
Matthew Lee <ml7@uw.edu>
2015-02-09 13:52:34 -0800

    Added Edward's changes to queue branch for queue.py.

--- queue.py
+++ queue.py
@@ -1,2 +1,17 @@
+class Node(object):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
 class Queue(object):
-    pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self._size = 0
+
+    def enqueue(self, val):
+        self.tail = Node(val, self.tail)
+        self._size += 1
+
+    def dequeue(self):
+        pass  
