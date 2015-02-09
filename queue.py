408242e83a752712c6eeaa288255d9813576e1d9
Edward <edpark13@gmail.com>
2015-02-09 14:08:08 -0800

    last commit before pull

--- queue.py
+++ queue.py
@@ -1,9 +1,17 @@
+class Node(object):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
 class Queue(object):
     def __init__(self):
-        pass
+        self.head = None
+        self.tail = None
+        self._size = 0
 
     def enqueue(self, val):
-        pass
+        self.tail = Node(val, self.tail)
+        self._size += 1
 
     def dequeue(self):
-        pass
+        pass  
