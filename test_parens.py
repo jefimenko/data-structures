1328d0916e527502e703f920334a0fe2b0de4cd3
Matthew Lee <ml7@uw.edu>
2015-02-09 01:39:36 -0800

    Added tests parens.py.

---
+++ test_parens.py
@@ -0,0 +1,36 @@
+from parens import balanceness
+
+def test_balanced():
+    # Simple case.
+    assert balanceness(u'()') is 0
+    # Extension of the simple case.
+    assert balanceness(u'((()))') is 0
+
+
+def test_balanced_longer():
+    assert balanceness(u'()()') is 0
+    assert balanceness(u'()()(())') is 0
+    assert balanceness(u'()(()())') is 0
+    assert balanceness(u'()(())()') is 0
+
+
+def test_open():
+    assert balanceness(u'(') is 1
+    assert balanceness(u'(()') is 1
+
+
+def test_open_longer():
+    assert balanceness(u'(())(') is 1
+    assert balanceness(u'()()(') is 1
+
+
+def test_broken():
+    assert balanceness(u')') is -1
+    assert balanceness(u'())') is -1
+    assert balanceness(u')()') is -1
+
+
+def test_broken_longer():
+    assert balanceness(u')))(((') is -1
+    assert balanceness(u'((())))') is -1
+    assert balanceness(u'()())()') is -1
