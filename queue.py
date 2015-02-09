ffff5f7ac80cd69dd528149e12c7fa3dfbb51c90
Matthew Lee <ml7@uw.edu>
2015-02-09 13:49:35 -0800

    Added changes Edward made to queue on master branch.

--- queue.py
+++ queue.py
@@ -1,17 +1,2 @@
-class Node(object):
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
 class Queue(object):
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self._size = 0
-
-    def enqueue(self, val):
-        self.tail = Node(val, self.tail)
-        self._size += 1
-
-    def dequeue(self):
-        pass  
+    pass
