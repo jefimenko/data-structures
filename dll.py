8f531041c3ad1515ef0e571887530f106b1929e2
Edward <edpark13@gmail.com>
2015-02-10 13:17:54 -0800

    add constructor for ListItem and DoublyLinkedList and pass tests

--- dll.py
+++ dll.py
@@ -1,6 +1,11 @@
 class ListItem(object):
-    pass
-
+    def __init__(self, data, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
 
 class DoublyLinkedList(object):
-    pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
