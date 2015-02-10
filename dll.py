ae17a293987810dac40906f4819fb4e5b6330c84
Matthew Lee <ml7@uw.edu>
2015-02-10 13:48:02 -0800

    Added append().

--- dll.py
+++ dll.py
@@ -19,10 +19,20 @@ class DoublyLinkedList(object):
         """
         insert function to insert a ListItem at head/front of DoublyLinkedList
         """
-        if self.head == None:
+        if self.head is None:
             # insert() on an empty list.
             self.head = self.tail = ListItem(val)
         else:
             # insert() on a list with items.
             self.head = ListItem(val, self.head)
             self.head.next.prev = self.head
+
+    def append(self, val):
+        """
+        Append a ListItem with at the tail/end of a DoublyLinkedList.
+        """
+        if self.head is None:
+            self.tail = self.head = ListItem(val)
+        else:
+            self.tail = ListItem(val, prev=self.tail)
+            self.tail.prev.next = self.tail
