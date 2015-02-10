e5abf8272df326c332ef34005c3b67b84f3e60c2
Edward <edpark13@gmail.com>
2015-02-10 13:22:56 -0800

    add insert() and pass test

--- dll.py
+++ dll.py
@@ -8,4 +8,9 @@ class DoublyLinkedList(object):
     def __init__(self):
         self.head = None
         self.tail = None
-        
+
+    def insert(self, val):
+        if self.head == None:
+            self.head = self.tail = ListItem(val)
+        else:
+            self.head = ListItem(val, self.head)
