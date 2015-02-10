329d7f761bd3c8da23fb848bcc29fbcb11c1673b
Edward <edpark13@gmail.com>
2015-02-10 13:25:03 -0800

    add comments for constructors and insert

--- dll.py
+++ dll.py
@@ -1,15 +1,24 @@
 class ListItem(object):
+    """
+    ListItem class to be used by DoublyLinkedList
+    """
     def __init__(self, data, next=None, prev=None):
         self.data = data
         self.next = next
         self.prev = prev
 
 class DoublyLinkedList(object):
+    """
+    DoublyLinkedList class using ListItem
+    """
     def __init__(self):
         self.head = None
         self.tail = None
 
     def insert(self, val):
+        """
+        insert function to insert a ListItem at head/front of DoublyLinkedList
+        """
         if self.head == None:
             self.head = self.tail = ListItem(val)
         else:
