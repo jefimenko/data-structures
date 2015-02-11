bf8c6352c081ee2a3ee4ec958967eb2c90ee8e7a
Matthew Lee <ml7@uw.edu>
2015-02-11 14:00:43 -0800

    Added push().

--- binheap.py
+++ binheap.py
@@ -1,16 +1,32 @@
 class Binheap(object):
-    def ___init___(self, vals=None):
-        self.list = List(vals)
+
+    def __init__(self, values=None):
+        """
+        Create a Binheap that stores values in a list.
+
+        If creating a populated Binheap, values must be an iterable
+        """
+        self.values = list()
+        if values:
+            # When you pass a string, the string will be taken apart as a list of characters
+            # to populate a heap if passed as values
+            try:
+                for val in values:
+                    self.push(val)
+            except TypeError:
+                raise TypeError('Binheap constructor values must be one iterable object.')
+
 
     def push(self, val):
-        self.list.append(val)
-        position = len(self.list) - 1
+        self.values.append(val)
+        position = len(self.values) - 1
         while position != 0:
             parent_position = (position - 1) / 2
-            if self.list[parent_position] < self.list[position]:
-                self.list[parent_position], self.list[position] = self.list[position], self.list[parent_position]
+            if self.values[parent_position] < self.values[position]:
+                self.values[parent_position], self.values[position] = self.values[position], self.values[parent_position]
                 position = parent_position
             else:
                 position = 0
 
     def pop(self, val):
+        pass
