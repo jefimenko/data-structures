d882bb1bcec604e91249b1615f9a56b5c569917a
Matthew Lee <ml7@uw.edu>
2015-02-12 13:02:09 -0800

    Added stubs for testing functions of Priority_Queue().

--- test_pq.py
+++ test_pq.py
@@ -1,9 +1,41 @@
 from priority_queue import Priority_Queue
+import pytest
 
 
-# Construct an empty Priority_Queue
+@pytest.fixture(scope='function')
+def m_eq(request):
+    return Priority_Queue
+
+
+@pytest.fixture(scope='function')
+def m_lbh(request):
+    q = Priority_Queue()
+    for x in range(5):
+        q.insert(x, 'low')
+    for x in range(-1, -6, -1):
+        q.insert(x, 'high')
+
+
+@pytest.fixture(scope='function')
+def m_hbl(request):
+    q = Priority_Queue()
+    for x in range(-1, -6, -1):
+        q.insert(x, 'high')
+    for x in range(5):
+        q.insert(x, 'low')
+
+
+# Test creating a Priority_Queue.
+def test_eq(m_eq):
+    assert isinstance(m_eq, Priority_Queue)
+    # Test that Priority_Queue is empty.
+    assert m_eq.high.size() == 0
+    assert m_eq.low.size() == 0
+
 
 # Test insert()
+def test_insert(m_eq, m_lbh, m_hbl):
+    pass
     # Onto empty Q
         # Low
         # High
@@ -11,12 +43,18 @@ from priority_queue import Priority_Queue
         # Low
         # High
 
+
 # Test pop()
+def test_pop(m_eq, m_lbh, m_hbl):
+    pass
     # Empty Q
     # Low before high
     # High before low
 
+
 # Test peek()
+def test_peek(m_eq, m_lbh, m_hbl):
+    pass
     # Empty Q
     # Low before high
     # High before low
