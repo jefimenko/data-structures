36f877a7012ea1cbad2edae9bb23b89e7d591620
Matthew Lee <ml7@uw.edu>
2015-02-12 12:52:52 -0800

    Added outline for tests.

---
+++ test_pq.py
@@ -0,0 +1,23 @@
+from priority_queue import Priority_Queue
+
+
+# Construct an empty Priority_Queue
+
+# Test insert()
+    # Onto empty Q
+        # Low
+        # High
+    # Onto non-empty Q
+        # Low
+        # High
+
+# Test pop()
+    # Empty Q
+    # Low before high
+    # High before low
+
+# Test peek()
+    # Empty Q
+    # Low before high
+    # High before low
+        # Does peek look at the next to be popped, or the next chronologically enqueued item?
