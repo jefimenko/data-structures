03221dbae64e383f76267dcfd8c08cf01eda3271
Edward <edpark13@gmail.com>
2015-02-16 12:35:46 -0800

    create file with Graph class and methods

---
+++ graph.py
@@ -0,0 +1,30 @@
+class Graph(object):
+    def __init__(self):
+        self.g = {}
+
+    def nodes(self):
+        pass
+
+    def edges(self):
+        pass
+
+    def add_node(self, n):
+        pass
+
+    def add_edge(self, n1, n2):
+        pass
+
+    def del_node(self, n):
+        pass
+
+    def del_edge(self, n1, n2):
+        pass
+
+    def has_node(self, n):
+        pass
+
+    def neighbors(self, n):
+        pass
+
+    def adjacent(self, n1, n2):
+        pass
