5c9ffbe2d29d65f3196693a728a98e9ce2f28e6b
Matthew Lee <ml7@uw.edu>
2015-02-19 15:49:36 -0800

    Tweaked tests for looped graphs.

--- test_graph.py
+++ test_graph.py
@@ -146,12 +146,16 @@ def test_breadth(popd_graph):
 def test_bfloop(popd_graph):
     g = popd_graph
     g.add_edge(10, 7)
+    g.add_edge(9, 2)
+    g.add_edge(9, 3)
     assert g.breadth_first_traversal(1) == '1234756910'
 
 
 def test_depthfloop(popd_graph):
     g = popd_graph
     g.add_edge(10, 7)
+    g.add_edge(9, 2)
+    g.add_edge(9, 3)
     assert g.depth_first_traversal(1) == '1245610937'
 
 
