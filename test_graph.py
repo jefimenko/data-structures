2ccb0234f1a94aacd5dabd75453b0ffd0226e56e
Edward <edpark13@gmail.com>
2015-03-03 16:44:25 -0800

    push for merge

--- test_graph.py
+++ test_graph.py
@@ -257,45 +257,47 @@ def test_find_paths():
     assert g.get_path_weight(['a','b','c']) == 2
     assert g.find_shortest_path('a', 'c') == (['a', 'c'], 1)
 
-# def test_real_short():
-#     g = Graph()
-#     g.add_node('a')
-#     g.add_edge('a', 'b', 9)
-#     g.add_edge('a', 'e', 3)
-#     g.add_edge('b', 'c', 6)
-#     g.add_edge('b', 'e', 9000)
-#     g.add_edge('c', 'd', 6)
-#     g.add_edge('e', 'c', 5)
-#     g.add_edge('e', 'd', 1)
-#     g.add_edge('e', 'f', 3)
-#     g.add_edge('f', 'a', 8)
-
-#     print g.find_paths('a','d',[],[])
-#     assert g.find_shortest_path('a', 'd') == (['a', 'e', 'd'], 4)
-
-
-# def test_real_long():
-#     g = Graph()
-#     g.add_node('a')
-#     g.add_edge('a', 'b', 1)
-#     g.add_edge('a', 'e', 9000)
-#     g.add_edge('b', 'c', 1)
-#     g.add_edge('c', 'e', 1)
-#     g.add_edge('c', 'd', 6)
-#     g.add_edge('e', 'd', 3)
-#     g.add_edge('e', 'f', 1)
-#     g.add_edge('f', 'd', 1)
-
-#     assert g.find_shortest_path('a', 'd') == (['a', 'b', 'c', 'e', 'f', 'd'], 5)
-
-def test_short():
+def test_real_short():
     g = Graph()
+    g.add_node('a')
+    g.add_edge('a', 'b', 9)
+    g.add_edge('a', 'e', 3)
+    g.add_edge('b', 'c', 6)
+    g.add_edge('b', 'e', 9000)
+    g.add_edge('c', 'd', 6)
+    g.add_edge('e', 'c', 5)
+    g.add_edge('e', 'd', 1)
+    g.add_edge('e', 'f', 3)
+    g.add_edge('f', 'a', 8)
+
+    print g.find_paths('a','d',[],[])
+    assert g.find_shortest_path('a', 'd') == (['a', 'e', 'd'], 4)
+
+
+def test_real_long():
+    g = Graph()
+    g.add_node('a')
     g.add_edge('a', 'b', 1)
-    g.add_edge('b', 'd', 1)
-    g.add_edge('a', 'c', 1)
-    g.add_edge('c', 'd', 3)
-    g.add_edge('d', 'e', 1)
-    assert g.find_shortest_path('a', 'e') == (['a','b','d','e'], 3)
+    g.add_edge('a', 'e', 9000)
+    g.add_edge('b', 'c', 1)
+    g.add_edge('c', 'e', 1)
+    g.add_edge('c', 'd', 6)
+    g.add_edge('e', 'd', 3)
+    g.add_edge('e', 'f', 1)
+    g.add_edge('f', 'd', 1)
+
+    assert g.find_shortest_path('a', 'd') == (['a', 'b', 'c', 'e', 'f', 'd'], 5)
+
+# def test_short():
+#     g = Graph()
+#     g.add_edge('a', 'b', 10000)
+#     g.add_edge('b', 'd', 1)
+#     g.add_edge('a', 'c', 1)
+#     g.add_edge('c', 'd', 2)
+#     g.add_edge('c', 'e', 1)
+#     g.add_edge('e', 'd', 2)
+#     g.add_edge('a', 'd', 3)
+#     assert g.find_shortest_path('a', 'd') == (['a','b','d'], 2)
 
 
 
