b9f3ad401188157b46109cfd04c270ff749de7c4
Henry Grantham <hgrantham@gmail.com>
2015-03-04 13:53:59 -0800

    add insert and test_bst.py with insert

---
+++ bst.py
@@ -0,0 +1,35 @@
+class Bst(object):
+    """Binary Search Tree"""
+    def __init__(self, value=None):
+        self.tree = {}
+        self.top = None
+        if not value is None:
+            self.tree[value] = {'depth': 1}
+            self.top = value
+
+    def insert(self, value):
+        current = self.top
+        if current is None:
+            self.tree[value] = {'depth': 1}
+            self.top = value
+            return
+        depth = 2
+        while not current is None:
+            print current
+            if current == value:
+                return
+            else:
+                if current < value:
+                    traverse = self.tree[current].get('right')
+                    child = 'right'
+                else:
+                    traverse = self.tree[current].get('left')
+                    child = 'left'
+                if traverse is None:
+                    #actual insert
+                    self.tree[value] = {'depth': depth}
+                    self.tree[current][child] = value
+                    return
+                else:
+                    current = traverse
+            depth +=1
---
+++ test_bst.py
@@ -0,0 +1,43 @@
+import bst
+import pytest
+
+
+def test_Bst():
+    """Constructing a bst tree"""
+    tree = bst.Bst()
+    assert tree.tree == {}
+    tree = bst.Bst(5)
+    assert tree.tree == {5: {'depth': 1}}
+
+
+def test_insert(empty_tree):
+    """Test insert into a tree"""
+    tree = empty_tree
+    tree.insert(20)
+    assert tree.tree == {20: {'depth': 1}}
+    tree.insert(30)
+    expect = {20: {'depth': 1,
+                   'right': 30},
+              30: {'depth': 2}}
+    assert tree.tree == expect
+    tree.insert(40) 
+    expect = {20: {'depth': 1,
+                   'right': 30},
+              30: {'depth': 2,
+                   'right': 40},
+              40: {'depth': 3}}
+    assert tree.tree == expect
+    tree.insert(10)
+    expect = {20: {'depth': 1,
+                   'left': 10,
+                   'right': 30},
+              30: {'depth': 2,
+                   'right': 40},
+              40: {'depth': 3},
+              10: {'depth': 2}}
+    assert tree.tree == expect
+
+@pytest.fixture(scope='function')
+def empty_tree():
+    tree = bst.Bst()
+    return tree
