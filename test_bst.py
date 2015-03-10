fb638b60728f4f39f8f441f2deeb49dfd08b03fc
Henry Grantham <hgrantham@gmail.com>
2015-03-10 13:27:34 -0700

    took out nodes from trees in test_bst.py

--- test_bst.py
+++ test_bst.py
@@ -7,34 +7,29 @@ def test_Bst():
     tree = bst.Bst()
     assert tree.tree == {}
     tree = bst.Bst(5)
-    assert tree.tree == {5: {'depth': 1}}
+    assert tree.tree == {5: {}}
 
 
 def test_insert(empty_tree):
     """Test insert into a tree"""
     tree = empty_tree
     tree.insert(20)
-    assert tree.tree == {20: {'depth': 1}}
+    assert tree.tree == {20: {}}
     tree.insert(30)
-    expect = {20: {'depth': 1,
-                   'right': 30},
-              30: {'depth': 2}}
+    expect = {20: {'right': 30},
+              30: {}}
     assert tree.tree == expect
     tree.insert(40) 
-    expect = {20: {'depth': 1,
-                   'right': 30},
-              30: {'depth': 2,
-                   'right': 40},
-              40: {'depth': 3}}
+    expect = {20: {'right': 30},
+              30: {'right': 40},
+              40: {}}
     assert tree.tree == expect
     tree.insert(10)
-    expect = {20: {'depth': 1,
-                   'left': 10,
+    expect = {20: {'left': 10,
                    'right': 30},
-              30: {'depth': 2,
-                   'right': 40},
-              40: {'depth': 3},
-              10: {'depth': 2}}
+              30: {'right': 40},
+              40: {},
+              10: {}}
     assert tree.tree == expect
 
 
