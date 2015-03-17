e7aa8714e3a1392bd780bd68f3b76597d32c1def
Henry Grantham <hgrantham@gmail.com>
2015-03-16 18:41:42 -0700

    added test with random sequence

--- test_insert_sort.py
+++ test_insert_sort.py
@@ -1,14 +1,22 @@
-from insert_sort import sort
+from insert_sort import insert_sort
+import random
 
 def test_ordered():
     something = [x for x in range(100)]
-    actual =  sort(something)
+    actual =  insert_sort(something)
     expected = [x for x in range(100)]
     assert actual == expected
 
 
 def test_reversed():
     something_else = [x for x in range(1000, 0, -1)]
-    actual = sort(something_else)
+    actual = insert_sort(something_else)
     expected = [x for x in range(1, 1001)]
+    assert actual == expected
+
+def test_random():
+    something_else = [x for x in range(1000)]
+    random.shuffle(something_else)
+    actual = insert_sort(something_else)
+    expected = [x for x in range(0, 1000)]
     assert actual == expected
