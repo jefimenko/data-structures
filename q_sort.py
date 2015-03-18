0d08d304f84b9c95e4a6df3b10744daa3b00ccd1
Matthew Lee <ml7@uw.edu>
2015-03-18 15:35:32 -0700

    Fixed general bugs. TODO: handle multiple occurences of values.

--- q_sort.py
+++ q_sort.py
@@ -1,21 +1,24 @@
 def q_sort(sequence): 
-    if len(sequence) == 1:
-        return 
+    if len(sequence) <= 1:
+        # Return a sequence of one sorted item, or stop if no input
+        return sequence
+    if len(sequence) == 2:
+        # Return a sequence of two sorted items
+        if sequence[1] > sequence[0]:
+            return sequence
+        else:
+            return sequence[::-1]
+
     pivot = get_pivot(sequence)
     left = []
     right = []
     for item in sequence:
-        if item > pivot and item is not pivot:
+        if item > pivot[0] and item is not pivot[0]:
             right.append(item)
-        else:
+        elif item is not pivot[0]:
             left.append(item)
-    right = q_sort(right)
-    left = q_sort(left)
 
-    if len(sequence) > 2:
-        return left + [pivot] + right
-    else:
-        return left + right
+    return q_sort(left)+ pivot + q_sort(right)
 
 def get_pivot(sequence):
     first = sequence[0]
@@ -24,7 +27,7 @@ def get_pivot(sequence):
     middle = sequence[mid_index]
 
     if last > first > middle or middle > first > last:
-        return first
+        return [first]
     if last > middle > first or first > middle > last:
-        return middle
-    return last
+        return [middle]
+    return [last]
