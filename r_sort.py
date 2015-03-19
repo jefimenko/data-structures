1f934cc16d3210f26e5355554fc88cde8dd10d96
Matthew Lee <ml7@uw.edu>
2015-03-19 15:07:29 -0700

    Changed way of handling negative numbers to use more bins.

--- r_sort.py
+++ r_sort.py
@@ -5,23 +5,19 @@ from merge_sort import timed_func
 def r_sort(sequence):
     digit = 1
     while True:
-        bins = [[] for i in range(10)]
+        pos_bins = [[] for i in range(10)]
+        neg_bins = [[] for i in range(10)]
         for x in sequence:
             # the expression in bins returns the number in digit place
-            bins[abs(x) % 10 ** digit // 10 ** (digit -1)].append(x)
-        sequence = [number for x in bins for number in x]
+            if x < 0:
+                neg_bins[abs(x) % 10 ** digit // 10 ** (digit -1)].append(x)
+            else:
+                pos_bins[x % 10 ** digit // 10 ** (digit -1)].append(x)
+        sequence = [number for x in neg_bins[::-1] for number in x] 
+        sequence += [number for x in pos_bins for number in x]
         digit += 1
-        if len(sequence) == len(bins[0]):
-
-            # Handling for negative numbers
-            final = []
-            for num in sequence:
-                if num < 0:
-                    final.insert(0, num)
-                else:
-                    final.append(num)
-
-            return final
+        if len(sequence) == len(pos_bins[0]) + len(neg_bins[0]):
+            return sequence
 
 if __name__ == '__main__':
     inputs = [range(500), range(1000)]
