0abc47c7aeb4d4c59e2ebe6e70d6aca7fdad2362
Henry Grantham <hgrantham@gmail.com>
2015-03-19 14:04:40 -0700

    fixed list comprehension that builds sequence and fixed error in index of bins

--- r_sort.py
+++ r_sort.py
@@ -7,8 +7,9 @@ def r_sort(sequence):
     while True:
         bins = [[] for i in range(10)]
         for x in sequence:
-            bins[x % 10 ** digit // 10 ** digit -1].append(x)
-        sequence = [number for x in bins for number in bins[x]]
+            # the expression in bins returns the number in digit place
+            bins[x % 10 ** digit // 10 ** (digit -1)].append(x)
+        sequence = [number for x in bins for number in x]
         digit += 1
         if len(sequence) == len(bins[0]):
             return sequence
@@ -16,7 +17,10 @@ def r_sort(sequence):
 
 if __name__ == '__main__':
     inputs = [range(500), range(1000)]
-
+    inputs.append([10 ** 1000, 1, 2, 3, 4, 5])
+    inputs.append(range(6))
+    inputs.append(range(1000, 0, -1))
+    inputs.append([10 ** 1000, 1, 2, 3, 4, 5] + range(1000, 0 -1))
 
     for inp in inputs:
         r_sort(inp)
