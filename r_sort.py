6e19d006856c5a198ce43eebf6675fde744c06ce
Henry Grantham <hgrantham@gmail.com>
2015-03-19 13:37:34 -0700

    added main block and timinf func

--- r_sort.py
+++ r_sort.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-from math import pow
 from merge_sort import timed_func
 
+@timed_func
 def r_sort(sequence):
     digit = 1
     while True:
@@ -10,4 +10,13 @@ def r_sort(sequence):
             bins[x % 10 ** digit // 10 ** digit -1].append(x)
         sequence = [number for x in bins for number in bins[x]]
         digit += 1
-    return sequence
+        if len(sequence) == len(bins[0]):
+            return sequence
+
+
+if __name__ == '__main__':
+    inputs = [range(500), range(1000)]
+
+
+    for inp in inputs:
+        r_sort(inp)
