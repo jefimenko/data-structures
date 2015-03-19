f288e96d9fcf3384e48a121c14c1926fa7787577
Henry Grantham <hgrantham@gmail.com>
2015-03-19 15:23:21 -0700

    added alpha sort tests

--- test_r_sort.py
+++ test_r_sort.py
@@ -1,4 +1,4 @@
-from r_sort import r_sort
+from r_sort import r_sort, r_sort_alpha
 
 
 def test_r_one():
@@ -17,3 +17,22 @@ def test_r_mixed():
     a = [1, 2, 5, 4, 4, 6, 7, 9, 8, 0, 0]
     a = r_sort(a)
     assert a == [0, 0, 1, 2, 4, 4, 5, 6, 7, 8, 9]
+
+
+def test_r_alpha_empty():
+    """Test empty strings"""
+    a = ['a', 'b', 'aaa', '']
+    a = r_sort_alpha(a)
+    assert a == ['', 'a', 'aaa', 'b']
+
+def test_r_alpha_shortvlong():
+    """Test that 6 character string comes before 5 char string"""
+    a = ['bbbaa', 'b', 'aaabbb', '']
+    a = r_sort_alpha(a)
+    assert a == ['', 'aaabbb', 'b', 'bbbaa']
+
+def test_r_alpha_zero_char():
+    """Test that 6 character string comes before 5 char string"""
+    a = ['\x00', 'b', 'c', '']
+    a = r_sort_alpha(a)
+    assert a == ['', '\x00', 'b', 'c']
